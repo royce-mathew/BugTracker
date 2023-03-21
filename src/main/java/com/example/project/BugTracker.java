767571bb84d035749e3d58f1a1422a2a2f478ff0
srivathsan-sivakumar-1 <sassysasquatch82@protonmail.com>
2023-03-21 10:55:30 -0400

    init maven project

---
+++ src/main/java/com/example/project/BugTracker.java
@@ -0,0 +1,9 @@
+/*
+ * Use this file for defining a BugTracker class. It should have attributes for
+ * the bug, progress, report, member assigned etc
+ */
+
+package com.example.project;
+
+public class BugTracker {
+}
---
+++ src/main/java/com/example/project/BugTrackerApplication.java
@@ -0,0 +1,11 @@
+/*
+ * Do not make changes to this file.
+ */
+package com.example.project;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class BugTrackerApplication extends Application{
+}
