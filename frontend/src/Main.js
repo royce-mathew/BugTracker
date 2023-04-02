49408f766d946201a22b5e6967a8b0ae80ca9053
Srivathsan Sivakumar <sri.skumar05@gmail.com>
2023-04-02 18:40:18 -0400

    center align components

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -5,30 +5,33 @@ import TextField from '@mui/material/TextField';
 class Main extends Component {
   render() {
     return (
-        <>
-            <h1>Bug Tracker</h1>
-            
-            {/* drop down list */}
-            <select>
-                <option value="user1">User 1</option>
-                <option value="user2">User 2</option>
-                <option value="user3">User 3</option>
-            </select>
-            
-            <br></br><br></br>
+        <>  
+            <center>
+                <h1>Bug Tracker</h1>
+                
+                {/* drop down list */}
+                <select>
+                    <option value="user1">User 1</option>
+                    <option value="user2">User 2</option>
+                    <option value="user3">User 3</option>
+                </select>
+                
+                <br></br><br></br>
 
-            {/* text box */}
-            <TextField
-                id="bug-description"
-                placeholder="Enter a short description of the bug..."
-                multiline
-                rows={4}
-                variant="filled"
-            />
-            <br></br><br></br>
+                {/* text box */}
+                <TextField
+                    id="bug-description"
+                    placeholder="Enter a short description of the bug..."
+                    multiline
+                    rows={4}
+                    variant="filled"
+                />
+                <br></br><br></br>
 
-            {/* submit button */}
-            <Button variant="contained">Submit</Button>
+                {/* submit button */}
+                <Button variant="contained">Submit</Button>
+            </center>
+            
         </>
     );
   }
