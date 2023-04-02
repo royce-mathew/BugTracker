ad96914f55d384a806461986a470cf50070e7d7c
Srivathsan Sivakumar <sri.skumar05@gmail.com>
2023-04-02 19:04:46 -0400

    update frontend components

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import { ListItemButton, ListItemText } from "@mui/material";
 
 class Main extends Component {
   render() {
@@ -9,6 +15,40 @@ class Main extends Component {
             <center>
                 <h1>Bug Tracker</h1>
                 
+                {/* drawer */}
+                <Drawer
+                    sx={{
+                    width: 200,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: 200,
+                        boxSizing: 'border-box',
+                    },
+                    }}
+                    variant="permanent"
+                    anchor="left"
+                >
+                    <Toolbar />
+                    <Divider />
+                    <List>
+
+                    <ListItemButton>
+                        <ListItemText primary="New Bug" />
+                    </ListItemButton>
+
+                    <ListItemButton>
+                        <ListItemText primary="Bug Logs" />
+                    </ListItemButton>
+
+                    <ListItemButton>
+                        <ListItemText primary="About" />
+                    </ListItemButton>
+
+                    </List>
+                </Drawer>
+                    <Toolbar />
+
+
                 {/* drop down list */}
                 <select>
                     <option value="user1">User 1</option>
@@ -31,7 +71,6 @@ class Main extends Component {
                 {/* submit button */}
                 <Button variant="contained">Submit</Button>
             </center>
-            
         </>
     );
   }
