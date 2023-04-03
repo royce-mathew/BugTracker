2400d36135aacd1bae07cf442c8643d96fc8dc53
royce-mathew <royce1mathew@gmail.com>
2023-04-02 22:53:46 -0400

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,13 @@ import importlib
 # Initialize a new Flask Application
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    return response
+
 @app.route(f"/")
 def index():
     print(f"Recieved Request on /api")
